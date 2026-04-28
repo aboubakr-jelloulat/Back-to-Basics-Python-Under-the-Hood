1aa9b20c18ff9a182aef066c81ab0f39e80f48d1
Aboubakr Jelloulat <abubakerjelloulat@gmail.com>
2026-04-28 15:26:22 +0100

    Exercise 0: Planting Your First Seed

---
+++ 01/ex0/ft_garden_intro.py
@@ -0,0 +1,8 @@
+def display_plant_information() -> None:
+    print('=== Welcome to My Garden ===')
+    print('Plant: Rose \nHeight: 25cm \nAge: 30 days')
+    print('=== End of Program ===')    
+
+
+if __name__ == "__main__":
+    display_plant_information()
